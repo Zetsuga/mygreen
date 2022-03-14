a6175a7efc1cf3121aaf838476ac8172b5b2d34a
Zetsuga <darkbear24@gmail.com>
2022-03-14 11:53:38 +0100

    actualizacion ruting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdministradorDomoticaComponent } from './pages/administrador-domotica/administrador-domotica.component';
+import { AdministradorInicioComponent } from './pages/administrador-inicio/administrador-inicio.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component: LandingComponent},
+  {path:"administrador/inicio" , component: AdministradorInicioComponent},
+  {path:"administrador/domotica" , component: AdministradorDomoticaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
