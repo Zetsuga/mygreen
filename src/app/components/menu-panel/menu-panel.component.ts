e8b936037f8532d1029bd3af7470bef3e3c4bc67
JuanJoseCM85 <juanjosecm85@gmail.com>
2022-03-15 00:53:56 +0100

    Subida Juanjod dia1-1

--- src/app/components/menu-panel/menu-panel.component.ts
+++ src/app/components/menu-panel/menu-panel.component.ts
@@ -10,19 +10,12 @@ import{faDoorClosed} from '@fortawesome/free-solid-svg-icons';
 })
 export class MenuPanelComponent implements OnInit {
 
-<<<<<<< HEAD
-  
-
-  constructor() {
-
-=======
   public faAddressBook;
   public faDoorClosed;
 
   constructor() {
     this.faAddressBook = faAddressBook;
     this.faDoorClosed = faDoorClosed;
->>>>>>> 36498eec706025d531a7cf4872e820c0ef6e8456
   }
 
   ngOnInit(): void {
