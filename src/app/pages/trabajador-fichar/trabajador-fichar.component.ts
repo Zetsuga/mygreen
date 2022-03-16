c62da5a9625810e549ceee43afc467d1cf25a323
JuanJoseCM85 <juanjosecm85@gmail.com>
2022-03-16 22:24:56 +0100

    Fin Fichar

--- src/app/pages/trabajador-fichar/trabajador-fichar.component.ts
+++ src/app/pages/trabajador-fichar/trabajador-fichar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from 'src/app/shared/usuario.service';
 
 @Component({
   selector: 'app-trabajador-fichar',
@@ -7,7 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrabajadorFicharComponent implements OnInit {
 
-  constructor() { }
+  public fichajes:{};
+  constructor(public usuario:UsuarioService) {
+    this.usuario.trabajador = true;
+     
+    this.fichajes = [
+      {
+        "fecha" : "15/02/2022",
+        "entrada" : "07:00",
+        "salida" : "14:32",
+      },{
+        "fecha" : "14/02/2022",
+        "entrada" : "07:00",
+        "salida" : "14:32",
+      },{
+        "fecha" : "13/02/2022",
+        "entrada" : "07:00",
+        "salida" : "14:32",
+      },{
+        "fecha" : "12/02/2022",
+        "entrada" : "07:00",
+        "salida" : "14:32",
+      },{
+        "fecha" : "11/02/2022",
+        "entrada" : "07:00",
+        "salida" : "14:32",
+      },{
+        "fecha" : "10/02/2022",
+        "entrada" : "07:00",
+        "salida" : "14:32",
+      },{
+        "fecha" : "09/02/2022",
+        "entrada" : "07:00",
+        "salida" : "14:32",
+      }
+    ]
+  }
 
   ngOnInit(): void {
   }
