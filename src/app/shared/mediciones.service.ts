3d6fea0e197664adb99c51a879e559226b78a402
JuanJoseCM85 <juanjosecm85@gmail.com>
2022-03-21 22:26:06 +0100

    modificaciones servicio mediciones

--- src/app/shared/mediciones.service.ts
+++ src/app/shared/mediciones.service.ts
@@ -18,9 +18,14 @@ export class MedicionesService {
   /*
     La función buscarRango retorna un rango de mediciones perteneciente al inicio y fin dado de una fecha
     Hay que pasarle como parametro un inicio y un fin que son dos fechas
+    Si inicio o fin son null llamamos al endpoint sin parametros para que nos devuelva el ultimo mes.
   */
   public buscarRango(inicio:Date,fin:Date):Observable<object>{
-    return this.http.get(this.url+ "?inicio="+inicio+"&fin="+fin)
+    //Comprobamos que inicio y fin no sean null
+    if(inicio!=null && fin != null)
+      return this.http.get(this.url+ "?inicio="+inicio+"&fin="+fin)
+    else
+      return this.http.get(this.url);
   }
 
 
