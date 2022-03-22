3acc45985d57d1af8091243c85a149d1f1979025
PedroAbLo <pedro.abenzalopez@gmail.com>
2022-03-22 23:29:34 +0100

    Funcionalidad usuarios y cambios varios front

--- src/app/pages/administrador-usuarios/administrador-usuarios.component.ts
+++ src/app/pages/administrador-usuarios/administrador-usuarios.component.ts
@@ -4,7 +4,7 @@ import { ToastService } from 'src/app/shared/toast.service';
 import { Usuario } from 'src/app/models/usuario';
 import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FincaService } from 'src/app/shared/finca.service';
 
 @Component({
   selector: 'app-administrador-usuarios',
@@ -13,95 +13,79 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class AdministradorUsuariosComponent implements OnInit {
 
-  public usuarios:{};
-  resetPasswordForm: FormGroup;
-  public usuario:Usuario
+  public usuarios:Usuario[];
+  public usuario:Usuario;
+  public botonFormulario:Boolean;
+  public indice:number;
 
-  constructor(public usuarioService:UsuarioService) {
 
-    this.usuario=new Usuario("","","","",0,"","","","","","");
+  constructor(public usuarioService:UsuarioService, private toastService:ToastService, private fincaService:FincaService,private router:Router) {
 
-    this.usuarios = [
-      {
-        "usuario_id":0,
-        "nombre":"Juan",
-        "apellidos":"Gil y Gil",
-        "telefono":"666 666 666",
-        "email":"usuario2@gmail.com",
-        "cargo":"Administrador"
-      },
-      {
-        "usuario_id":0,
-        "nombre":"José",
-        "apellidos":"López Martínez",
-        "telefono":"555 666 666",
-        "email":"usuario3@gmail.com",
-        "cargo":"Administrador"
-      },
-      {
-        "usuario_id":0,
-        "nombre":"Pedro",
-        "apellidos":"Guillén Morientes",
-        "telefono":"444 666 666",
-        "email":"usuario4@gmail.com",
-        "cargo":"trabajador"
-      },
-      {
-        "usuario_id":0,
-        "nombre":"Judith",
-        "apellidos":"Hernández Abenza",
-        "telefono":"333 666 666",
-        "email":"usuario5@gmail.com",
-        "cargo":"trabajador"
-      },
-      {
-        "usuario_id":0,
-        "nombre":"Adrian",
-        "apellidos":"Naranja Murcia",
-        "telefono":"222 666 666",
-        "email":"usuario6@gmail.com",
-        "cargo":"trabajador"
-      },
-      {
-        "usuario_id":0,
-        "nombre":"María",
-        "apellidos":"Freeman Contreras",
-        "telefono":"111 666 666",
-        "email":"usuario7@gmail.com",
-        "cargo":"trabajador"
-      }
-    ]
+    this.botonFormulario=true
+    this.usuario=new Usuario("","","","",0,"","","","4","","");
+    this.usuarioService.buscarTodos(this.fincaService.finca.id_finca).subscribe((datos:any)=>{
+      this.usuarios=datos.resultado;
+    })
+
    }
-
+   
    public onSubmit(form:NgForm){
+    console.log(this.usuario);
+     this.usuarioService.crear(this.usuario).subscribe((datos:any)=>{
+       if(datos.error==true){
+         this.toastService.showError(datos.mensaje,datos.titulo);
+       }else{
+         this.toastService.showOk(datos.mensaje, datos.titulo);
+       }
+
+    })
 
    }
+
+   public cargarDatos(id){
+     this.usuario = this.usuarios[id];
+     this.botonFormulario = false;
+     this.indice=id;
+   }
+
+   public eliminarUsuario(id){
+     console.log(this.usuarios[id].id_usuario)
+     this.usuarioService.eliminar(this.usuarios[id].id_usuario).subscribe((datos:any)=>{
+      if(datos.error==true){
+        this.toastService.showError(datos.mensaje,datos.titulo);
+      }else{
+        this.toastService.showOk(datos.mensaje, datos.titulo);
+        this.usuarios.splice(id,1);
+        this.botonFormulario=true;
+      }
+     })
+   }
+
+   public modificarUsuario(){
+     this.usuarioService.modificar(this.usuario).subscribe((datos:any)=>{
+      if(datos.error==true){
+        this.toastService.showError(datos.mensaje,datos.titulo);
+      }else{
+        this.toastService.showOk(datos.mensaje, datos.titulo);
+        this.usuarios[this.indice]=this.usuario;
+        this.botonFormulario=true;
+      }
+     })
+   }
+
+
 
   ngOnInit(): void {
-
-    this.createResetPasswordForm();
-
+    if(this.usuarioService.logueado==false && this.usuarioService.usuario.rol!="2"){
+      this.router.navigateByUrl('/login');
+    }
   }
 
-  createResetPasswordForm(): void {
-    this.resetPasswordForm = new FormGroup({
-      currentPassword: new FormControl('', Validators.required),
-      newPassword: new FormControl('', Validators.required),
-      passwordConfirm: new FormControl('', Validators.required)
-    })
-  }
 
-  get currentPasswordIsValid() {
-    return this.resetPasswordForm.get('currentPassword').valid;
-  }
-  
-  get newPasswordIsValid() {
-    return this.resetPasswordForm.get('newPassword').valid;
-  }
-  
-  get passwordConfirmIsValid() {
-    return this.resetPasswordForm.get('passwordConfirm').valid;
-  }
-
+  // Validación entre campos password
+  // passwordMatchValidator(g: FormGroup) {
+  //   return g.get('newPassword').value === g.get('passwordConfirm').value
+  //       ? null : { 'mismatch': true };
+  // }
 
 }
