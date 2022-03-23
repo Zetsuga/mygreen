efce45774cae7a81f88b58dea70e9b9fbdd29b96
Zetsuga <darkbear24@gmail.com>
2022-03-23 07:52:57 +0100

    creado servicio usuarioFinca

---
+++ src/app/models/usuario-finca.ts
@@ -0,0 +1,7 @@
+export class UsuarioFinca {
+
+    public id_usuarios_fincas:number;
+    constructor(public id_usuario:number,public id_fnca:number){
+        this.id_usuarios_fincas = 0;
+    }
+}
--- src/app/shared/finca.service.ts
+++ src/app/shared/finca.service.ts
@@ -1,5 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Finca } from '../models/finca';
+import { UsuarioFinca } from '../models/usuario-finca';
 
 @Injectable({
   providedIn: 'root'
@@ -7,7 +10,17 @@ import { Finca } from '../models/finca';
 export class FincaService {
 
   public finca:Finca;
-  constructor() {
+  public url:string;
+  public usuarioFinca:UsuarioFinca;
+
+  constructor(private http:HttpClient) {
     this.finca = new Finca("",0,"","",0,0);
+
+    this.url =`https://mygreenapi.herokuapp.com/usuario`;
    }
+
+   public insertarUsuarioFinca(id_usuario:number,id_finca:number):Observable<object>{
+    this.usuarioFinca = new UsuarioFinca(id_usuario,id_finca);
+    return this.http.post(this.url,this.usuarioFinca);
+  }
 }
