c9cebefb6783e68c4c7e3602a22087c34eb22cfd
PedroAbLo <pedro.abenzalopez@gmail.com>
2022-03-24 13:44:08 +0100

    pagina trabajador fichar

--- src/app/models/fichar.ts
+++ src/app/models/fichar.ts
@@ -1,2 +1,9 @@
+import { Time } from "@angular/common";
+
 export class Fichar {
+    public id_fichaje:number;
+
+    constructor(public id_usuario:number,public fecha:Date, public entrada:string, public salida:string){
+        this.id_fichaje=0;
+    }
 }
