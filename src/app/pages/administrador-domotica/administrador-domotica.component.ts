39251ef37bd72848ea78a85df1effc88263bdf0d
JuanJoseCM85 <juanjosecm85@gmail.com>
2022-03-24 09:30:13 +0100

    Retocando web actuadores

--- src/app/pages/administrador-domotica/administrador-domotica.component.ts
+++ src/app/pages/administrador-domotica/administrador-domotica.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UsuarioService } from 'src/app/shared/usuario.service';
+import { Router } from '@angular/router';
+import { MedicionesService } from 'src/app/shared/mediciones.service';
+import { number } from 'echarts';
 
 @Component({
   selector: 'app-administrador-domotica',
@@ -10,15 +13,53 @@ export class AdministradorDomoticaComponent implements OnInit {
 
   mensaje = 1;
   mensaje2 = 2;
+  public tension: number;
+  public temperatura: number;
+  public humedad: number;
+  public isLoading: boolean;
+  public options:any;
+  public dataTension = [];
+  public dataTemperatura = [];
+  public dataHumedad = [];
 
-  constructor(public usuario:UsuarioService) {
+  constructor(public usuario:UsuarioService, private router:Router, public mediciones:MedicionesService) {
    }
 
   ngOnInit(): void {
-  }
+    if(this.usuario.logueado==false && this.usuario.usuario.rol!="2"){
+      this.router.navigateByUrl('/login');
+    }
 
-  isLoading = false;
-  options = {
+    this.mediciones.buscar().subscribe((datos:any)=>{
+      this.tension = datos.resultado[datos.resultado.length-1].tensionmatricial;
+    })
+
+    this.mediciones.buscar().subscribe((datos:any)=>{
+      this.tension = datos.resultado[datos.resultado.length-1].tensionmatricial;
+      this.temperatura = datos.resultado[datos.resultado.length-1].temperatura;
+      this.humedad = datos.resultado[datos.resultado.length-1].humedad;
+    })
+
+    this.mediciones.buscarRango(null,null).subscribe((datos:any)=>{
+      console.log(datos);
+
+      for( let i = datos.resultado.length-1; i>datos.resultado.length - 8 ; i--){
+        this.dataTension.push(datos.resultado[i].tensionmatricial);
+        this.dataTemperatura.push(datos.resultado[i].temperatura);
+        this.dataHumedad.push(datos.resultado[i].humedad);
+
+      }
+
+      console.log(this.dataTension);
+      console.log(this.dataTemperatura);
+      console.log(this.dataHumedad);
+      
+      
+      
+
+      // Pintamos la gráfica
+    this.isLoading = false;
+    this.options = {
     tooltip: {
       trigger: 'axis',
       axisPointer: {
@@ -55,23 +96,25 @@ export class AdministradorDomoticaComponent implements OnInit {
         type: 'line',
         stack: 'counts',
         areaStyle: { normal: {} },
-        data: [120, 132, 101, 134, 90, 230, 210]
+        data: this.dataTension
       },
       {
         name: 'Temp.',
         type: 'line',
         stack: 'counts',
         areaStyle: { normal: {} },
-        data: [220, 182, 191, 234, 290, 330, 310]
+        data: this.dataTemperatura
       },
       {
         name: 'Humedad',
         type: 'line',
         stack: 'counts',
         areaStyle: { normal: {} },
-        data: [150, 232, 201, 154, 190, 330, 410]
+        data: this.dataHumedad
       },
     ]
   };
+    })
+  }
 
 }
