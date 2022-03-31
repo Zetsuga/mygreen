88467e0d80fe637d3d4fe41b4f58ec676c91e383
Zetsuga <darkbear24@gmail.com>
2022-03-31 09:01:08 +0200

    Añadido en el super adminsitrador la gestión de fincas

--- src/app/models/finca.ts
+++ src/app/models/finca.ts
@@ -2,7 +2,7 @@ export class Finca {
 
     public id_finca:number;
 
-    constructor(public direccion:string,public cp:number,public poblacion:string, public provincia:string,public latitud:number, public logitud:number){
+    constructor(public direccion:string,public cp:number,public poblacion:string, public provincia:string,public latitud:number, public longitud:number){
         this.id_finca = 0;
     }
 }
